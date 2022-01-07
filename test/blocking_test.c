ba303f9a96ab6ab9cc198bee66d96bdf08afbd80
Brian Lopez <seniorlopez@gmail.com>
2022-01-07 14:29:42 -0800

    Add some prepared statement blocking API tests

--- test/blocking_test.c
+++ test/blocking_test.c
@@ -165,6 +165,150 @@ TEST test_blocking_query_no_rows()
     PASS();
 }
 
+TEST test_blocking_stmt_prepare()
+{
+    trilogy_conn_t conn;
+
+    connect_conn(&conn);
+
+    const char *query = "SELECT ?";
+    trilogy_stmt_t stmt;
+
+    int err = trilogy_stmt_prepare(&conn, query, strlen(query), &stmt);
+    ASSERT_OK(err);
+
+    ASSERT_EQ(1, stmt.parameter_count);
+
+    trilogy_column_packet_t param;
+    err = trilogy_read_full_column(&conn, &param);
+    ASSERT_OK(err);
+
+    ASSERT_EQ(1, stmt.column_count);
+
+    trilogy_column_packet_t column_def;
+    err = trilogy_read_full_column(&conn, &column_def);
+    ASSERT_OK(err);
+
+    trilogy_free(&conn);
+    PASS();
+}
+
+TEST test_blocking_stmt_execute()
+{
+    trilogy_conn_t conn;
+
+    connect_conn(&conn);
+
+    const char *query = "SELECT ?";
+    trilogy_stmt_t stmt;
+
+    int err = trilogy_stmt_prepare(&conn, query, strlen(query), &stmt);
+    ASSERT_OK(err);
+
+    ASSERT_EQ(1, stmt.parameter_count);
+
+    trilogy_column_packet_t param;
+    err = trilogy_read_full_column(&conn, &param);
+    ASSERT_OK(err);
+
+    ASSERT_EQ(1, stmt.column_count);
+
+    trilogy_column_packet_t column_def;
+    err = trilogy_read_full_column(&conn, &column_def);
+    ASSERT_OK(err);
+
+    uint8_t flags = 0x00;
+    const char str[] = {'t', 'e', 's', 't'};
+    size_t len = sizeof(str);
+    trilogy_binary_value_t binds[] = {
+        {.is_null = false, .type = TRILOGY_TYPE_VAR_STRING, .as.str.data = str, .as.str.len = len}};
+
+    uint64_t column_count;
+
+    err = trilogy_stmt_execute(&conn, &stmt, flags, binds, &column_count);
+    ASSERT_OK(err);
+
+    ASSERT_EQ(1, column_count);
+
+    trilogy_column_packet_t columns[1];
+    err = trilogy_read_full_column(&conn, &columns[0]);
+    ASSERT_OK(err);
+
+    trilogy_binary_value_t values[1];
+    err = trilogy_stmt_read_full_row(&conn, &stmt, columns, values);
+    ASSERT_OK(err);
+
+    ASSERT_MEM_EQ(values[0].as.str.data, "test", values[0].as.str.len);
+
+    err = trilogy_stmt_read_full_row(&conn, &stmt, columns, values);
+    ASSERT_EOF(err);
+
+    trilogy_free(&conn);
+    PASS();
+}
+
+TEST test_blocking_stmt_reset()
+{
+    trilogy_conn_t conn;
+
+    connect_conn(&conn);
+
+    const char *query = "SELECT ?";
+    trilogy_stmt_t stmt;
+
+    int err = trilogy_stmt_prepare(&conn, query, strlen(query), &stmt);
+    ASSERT_OK(err);
+
+    ASSERT_EQ(1, stmt.parameter_count);
+
+    trilogy_column_packet_t param;
+    err = trilogy_read_full_column(&conn, &param);
+    ASSERT_OK(err);
+
+    ASSERT_EQ(1, stmt.column_count);
+
+    trilogy_column_packet_t column_def;
+    err = trilogy_read_full_column(&conn, &column_def);
+    ASSERT_OK(err);
+
+    err = trilogy_stmt_reset(&conn, &stmt);
+    ASSERT_OK(err);
+
+    trilogy_free(&conn);
+    PASS();
+}
+
+TEST test_blocking_stmt_close()
+{
+    trilogy_conn_t conn;
+
+    connect_conn(&conn);
+
+    const char *query = "SELECT ?";
+    trilogy_stmt_t stmt;
+
+    int err = trilogy_stmt_prepare(&conn, query, strlen(query), &stmt);
+    ASSERT_OK(err);
+
+    ASSERT_EQ(1, stmt.parameter_count);
+
+    trilogy_column_packet_t param;
+    err = trilogy_read_full_column(&conn, &param);
+    ASSERT_OK(err);
+
+    ASSERT_EQ(1, stmt.column_count);
+
+    trilogy_column_packet_t column_def;
+    err = trilogy_read_full_column(&conn, &column_def);
+    ASSERT_OK(err);
+
+    err = trilogy_stmt_close(&conn, &stmt);
+    ASSERT_OK(err);
+
+    trilogy_free(&conn);
+    PASS();
+}
+
 int blocking_test()
 {
     RUN_TEST(test_blocking_connect);
@@ -174,6 +318,10 @@ int blocking_test()
     RUN_TEST(test_blocking_query);
     RUN_TEST(test_blocking_query_error);
     RUN_TEST(test_blocking_query_no_rows);
+    RUN_TEST(test_blocking_stmt_prepare);
+    RUN_TEST(test_blocking_stmt_execute);
+    RUN_TEST(test_blocking_stmt_reset);
+    RUN_TEST(test_blocking_stmt_close);
 
     return 0;
 }
