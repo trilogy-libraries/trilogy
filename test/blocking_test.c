9844b48e665d8d3ec1c27bb069c95efad4e533bd
Brian Lopez <seniorlopez@gmail.com>
2022-01-31 21:25:29 -0800

    Add a few more binary protocol tests

--- test/blocking_test.c
+++ test/blocking_test.c
@@ -193,7 +193,7 @@ TEST test_blocking_stmt_prepare()
     PASS();
 }
 
-TEST test_blocking_stmt_execute()
+TEST test_blocking_stmt_execute_str()
 {
     trilogy_conn_t conn;
 
@@ -239,6 +239,184 @@ TEST test_blocking_stmt_execute()
     ASSERT_OK(err);
 
     ASSERT_MEM_EQ(values[0].as.str.data, "test", values[0].as.str.len);
+
+    err = trilogy_stmt_read_full_row(&conn, &stmt, columns, values);
+    ASSERT_EOF(err);
+
+    trilogy_free(&conn);
+    PASS();
+}
+
+TEST test_blocking_stmt_execute_integer()
+{
+    trilogy_conn_t conn;
+
+    connect_conn(&conn);
+
+    const char *query = "SELECT ?";
+    trilogy_stmt_t stmt;
+
+    int err = trilogy_stmt_prepare(&conn, query, strlen(query), &stmt);
+    ASSERT_OK(err);
+
+    ASSERT_EQ(1, stmt.parameter_count);
+
+    trilogy_column_packet_t param;
+    err = trilogy_read_full_column(&conn, &param);
+    ASSERT_OK(err);
+
+    ASSERT_EQ(1, stmt.column_count);
+
+    trilogy_column_packet_t column_def;
+    err = trilogy_read_full_column(&conn, &column_def);
+    ASSERT_OK(err);
+
+    uint8_t flags = 0x00;
+    uint32_t unsigned_val = 1234;
+    trilogy_binary_value_t binds[] = {
+        {.is_null = false, .is_unsigned = true, .type = TRILOGY_TYPE_LONG, .as.uint32 = unsigned_val}};
+
+    uint64_t column_count;
+
+    err = trilogy_stmt_execute(&conn, &stmt, flags, binds, &column_count);
+    ASSERT_OK(err);
+
+    ASSERT_EQ(1, column_count);
+
+    trilogy_column_packet_t columns[1];
+    err = trilogy_read_full_column(&conn, &columns[0]);
+    ASSERT_OK(err);
+
+    trilogy_binary_value_t values[1];
+    err = trilogy_stmt_read_full_row(&conn, &stmt, columns, values);
+    ASSERT_OK(err);
+
+    ASSERT_EQ(values[0].as.uint32, unsigned_val);
+
+    err = trilogy_stmt_read_full_row(&conn, &stmt, columns, values);
+    ASSERT_EOF(err);
+
+    int32_t signed_val = -1234;
+
+    trilogy_binary_value_t signed_binds[] = {
+        {.is_null = false, .is_unsigned = false, .type = TRILOGY_TYPE_LONG, .as.int32 = signed_val}};
+
+    err = trilogy_stmt_execute(&conn, &stmt, flags, signed_binds, &column_count);
+    ASSERT_OK(err);
+
+    ASSERT_EQ(1, column_count);
+
+    err = trilogy_read_full_column(&conn, &columns[0]);
+    ASSERT_OK(err);
+
+    err = trilogy_stmt_read_full_row(&conn, &stmt, columns, values);
+    ASSERT_OK(err);
+
+    ASSERT_EQ(values[0].as.int32, signed_val);
+
+    err = trilogy_stmt_read_full_row(&conn, &stmt, columns, values);
+    ASSERT_EOF(err);
+
+    trilogy_free(&conn);
+    PASS();
+}
+
+TEST test_blocking_stmt_execute_double()
+{
+    trilogy_conn_t conn;
+
+    connect_conn(&conn);
+
+    const char *query = "SELECT ?";
+    trilogy_stmt_t stmt;
+
+    int err = trilogy_stmt_prepare(&conn, query, strlen(query), &stmt);
+    ASSERT_OK(err);
+
+    ASSERT_EQ(1, stmt.parameter_count);
+
+    trilogy_column_packet_t param;
+    err = trilogy_read_full_column(&conn, &param);
+    ASSERT_OK(err);
+
+    ASSERT_EQ(1, stmt.column_count);
+
+    trilogy_column_packet_t column_def;
+    err = trilogy_read_full_column(&conn, &column_def);
+    ASSERT_OK(err);
+
+    uint8_t flags = 0x00;
+    double dbl_val = 1234.5;
+    trilogy_binary_value_t binds[] = {
+        {.is_null = false, .is_unsigned = true, .type = TRILOGY_TYPE_DOUBLE, .as.dbl = dbl_val}};
+
+    uint64_t column_count;
+
+    err = trilogy_stmt_execute(&conn, &stmt, flags, binds, &column_count);
+    ASSERT_OK(err);
+
+    ASSERT_EQ(1, column_count);
+
+    trilogy_column_packet_t columns[1];
+    err = trilogy_read_full_column(&conn, &columns[0]);
+    ASSERT_OK(err);
+
+    trilogy_binary_value_t values[1];
+    err = trilogy_stmt_read_full_row(&conn, &stmt, columns, values);
+    ASSERT_OK(err);
+
+    ASSERT_EQ(values[0].as.dbl, dbl_val);
+
+    err = trilogy_stmt_read_full_row(&conn, &stmt, columns, values);
+    ASSERT_EOF(err);
+
+    trilogy_free(&conn);
+    PASS();
+}
+
+TEST test_blocking_stmt_execute_datetime()
+{
+    trilogy_conn_t conn;
+
+    connect_conn(&conn);
+
+    const char *query = "SELECT CAST('2022-01-31 21:15:45' AS DATETIME)";
+    trilogy_stmt_t stmt;
+
+    int err = trilogy_stmt_prepare(&conn, query, strlen(query), &stmt);
+    ASSERT_OK(err);
+
+    ASSERT_EQ(0, stmt.parameter_count);
+
+    ASSERT_EQ(1, stmt.column_count);
+
+    trilogy_column_packet_t column_def;
+    err = trilogy_read_full_column(&conn, &column_def);
+    ASSERT_OK(err);
+
+    uint8_t flags = 0x00;
+
+    uint64_t column_count;
+
+    err = trilogy_stmt_execute(&conn, &stmt, flags, NULL, &column_count);
+    ASSERT_OK(err);
+
+    ASSERT_EQ(1, column_count);
+
+    trilogy_column_packet_t columns[1];
+    err = trilogy_read_full_column(&conn, &columns[0]);
+    ASSERT_OK(err);
+
+    trilogy_binary_value_t values[1];
+    err = trilogy_stmt_read_full_row(&conn, &stmt, columns, values);
+    ASSERT_OK(err);
+
+    ASSERT_EQ(values[0].as.date.year, 2022);
+    ASSERT_EQ(values[0].as.date.month, 1);
+    ASSERT_EQ(values[0].as.date.day, 31);
+    ASSERT_EQ(values[0].as.date.datetime.hour, 21);
+    ASSERT_EQ(values[0].as.date.datetime.minute, 15);
+    ASSERT_EQ(values[0].as.date.datetime.second, 45);
 
     err = trilogy_stmt_read_full_row(&conn, &stmt, columns, values);
     ASSERT_EOF(err);
@@ -319,7 +497,10 @@ int blocking_test()
     RUN_TEST(test_blocking_query_error);
     RUN_TEST(test_blocking_query_no_rows);
     RUN_TEST(test_blocking_stmt_prepare);
-    RUN_TEST(test_blocking_stmt_execute);
+    RUN_TEST(test_blocking_stmt_execute_str);
+    RUN_TEST(test_blocking_stmt_execute_integer);
+    RUN_TEST(test_blocking_stmt_execute_double);
+    RUN_TEST(test_blocking_stmt_execute_datetime);
     RUN_TEST(test_blocking_stmt_reset);
     RUN_TEST(test_blocking_stmt_close);
 
