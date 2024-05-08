e0ef9dd2d999115d327f6d9ece3c943487872208
eileencodes <eileencodes@gmail.com>
2024-05-08 16:07:00 -0400

    Fix typo

--- src/client.c
+++ src/client.c
@@ -305,7 +305,7 @@ int trilogy_connect_send_socket(trilogy_conn_t *conn, trilogy_sock_t *sock)
         return rc;
 
     conn->socket = sock;
-    conn->packet_parser.sequence_numer = 0;
+    conn->packet_parser.sequence_number = 0;
 
     return TRILOGY_OK;
 }
