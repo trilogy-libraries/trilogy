b4280737cd10ee8bc84ed22e0b6c2ee3508a74b1
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-10-25 18:20:03 +1300

    Use `rb_io_wait` function and cache `io` instance.

--- contrib/ruby/ext/trilogy-ruby/extconf.rb
+++ contrib/ruby/ext/trilogy-ruby/extconf.rb
@@ -18,4 +18,8 @@ have_library("crypto", "CRYPTO_malloc")
 have_library("ssl", "SSL_new")
 have_func("rb_interned_str", "ruby.h")
 
+have_func("rb_io_wait", "ruby.h")
+have_func("rb_io_open_descriptor", "ruby.h")
+have_header("ruby/fiber/scheduler.h")
+
 create_makefile "trilogy/cext"
