e048eff6ac1194978b00f44d2f0986b7f212c294
Pavel Skrylev <majioa@altlinux.org>
2025-02-05 18:13:01 +0300

    feat: use precompiled so/a library to build with it a ruby source extension
    * use precompiled so/a library to build with it a ruby source extension

--- contrib/ruby/ext/trilogy-ruby/extconf.rb
+++ contrib/ruby/ext/trilogy-ruby/extconf.rb
@@ -1,15 +1,6 @@
 require "mkmf"
 
-# concatenate trilogy library sources to allow the compiler to optimise across
-# source files
-
-trilogy_src_dir = File.realpath("src", __dir__)
-File.binwrite("trilogy.c",
-  Dir["#{trilogy_src_dir}/**/*.c"].map { |src|
-    %{#line 1 "#{src}"\n} + File.binread(src)
-  }.join)
-
-$objs = %w[trilogy.o cast.o cext.o]
+$objs = %w[cast.o cext.o]
 $CFLAGS << " -I #{__dir__}/inc -std=gnu99 -fvisibility=hidden"
 
 dir_config("openssl")
@@ -17,5 +8,8 @@ dir_config("openssl")
 have_library("crypto", "CRYPTO_malloc")
 have_library("ssl", "SSL_new")
 have_func("rb_interned_str", "ruby.h")
+root = File.expand_path(File.join(__dir__, "../../../../"))
+dirs = Dir["#{root}/**/*.{a,so}"].map {|x|File.dirname(x)} | [root]
+find_library("trilogy", nil, *dirs)
 
 create_makefile "trilogy/cext"
