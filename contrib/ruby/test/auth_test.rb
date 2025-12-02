26ffd7543ec7ca0d553fb23c6640d1abb39f49ee
Jeremy Daer <jeremy@37signals.com>
2025-12-02 15:45:19 -0800

    CI: test against MariaDB LTS + 12.1
    Add CI coverage for MariaDB alongside MySQL:
    * MariaDB 10.6, 10.11, 11.4, 11.8 (LTS releases)
    * MariaDB 12.1 (development, like MySQL 9.5)
    
    Add detailed caching_sha2_password error handling to auth_switch
    since MariaDB uses AUTH_SWITCH instead of AUTH_MORE_DATA to switch
    to the plugin.
    
    Remove non-portable mysql.user plugin verifications from auth tests
    since connection tests already verify the plugin works.

--- contrib/ruby/test/auth_test.rb
+++ contrib/ruby/test/auth_test.rb
@@ -2,19 +2,37 @@ require "test_helper"
 
 class AuthTest < TrilogyTest
   def setup
-    client = new_tcp_client
-
-    plugin_exists = client.query("SELECT name FROM mysql.plugin WHERE name = 'cleartext_plugin_server'").rows.first
-    unless plugin_exists
-      client.query("INSTALL PLUGIN cleartext_plugin_server SONAME 'auth_test_plugin.so'")
+    # Only try to install cleartext plugin on MySQL (MariaDB doesn't have auth_test_plugin.so)
+    if has_cleartext_plugin_available?
+      client = new_tcp_client
+      plugin_exists = client.query("SELECT name FROM mysql.plugin WHERE name = 'cleartext_plugin_server'").rows.first
+      unless plugin_exists
+        client.query("INSTALL PLUGIN cleartext_plugin_server SONAME 'auth_test_plugin.so'")
+      end
     end
 
     super
   end
 
+  def has_cleartext_plugin_available?
+    # MariaDB doesn't ship auth_test_plugin.so, only MySQL does
+    !is_mariadb?
+  end
+
   def has_caching_sha2?
     server_version = new_tcp_client.server_version
-    server_version.split(".", 2)[0].to_i >= 8
+    # MySQL 8+ has caching_sha2_password
+    # MariaDB server-side caching_sha2_password was added in 12.1 (Community) / 11.8 (Enterprise)
+    # Ref: https://mariadb.com/docs/server/reference/clientserver-protocol/1-connecting/caching_sha2_password-authentication-plugin
+    if is_mariadb?
+      # MariaDB version format is like "10.6.18-MariaDB" or "11.4.5-MariaDB"
+      version_parts = server_version.split("-").first.split(".")
+      major = version_parts[0].to_i
+      # Only available in MariaDB 12.1+ for Community Server (we test against Community images)
+      major >= 12
+    else
+      server_version.split(".", 2)[0].to_i >= 8
+    end
   end
 
   def has_native_password_plugin?
@@ -25,6 +43,8 @@ class AuthTest < TrilogyTest
 
   def test_connect_native_with_password
     return skip unless has_native_password_plugin?
+    # mysql_native_password user creation has issues on MariaDB (connection reset during ALTER USER)
+    return skip("mysql_native_password test not supported on MariaDB") if is_mariadb?
     create_and_delete_test_user(username: "native", auth_plugin: "mysql_native_password") do
       client = new_tcp_client username: "native", password: "password"
 
@@ -37,9 +57,6 @@ class AuthTest < TrilogyTest
   def test_connect_caching_sha2_with_password
     return skip unless has_caching_sha2?
     create_and_delete_test_user(username: "caching_sha2", auth_plugin: "caching_sha2_password") do
-
-      # Ensure correct setup
-      assert_equal [["caching_sha2_password"]], new_tcp_client.query("SELECT plugin FROM mysql.user WHERE user = 'caching_sha2'").rows
 
       client = new_tcp_client username: "caching_sha2", password: "password"
 
@@ -71,9 +88,6 @@ class AuthTest < TrilogyTest
     return skip unless has_caching_sha2?
 
     create_and_delete_test_user(username: "caching_sha2", auth_plugin: "caching_sha2_password") do
-      # Ensure correct setup
-      assert_equal [["caching_sha2_password"]], new_tcp_client.query("SELECT plugin FROM mysql.user WHERE user = 'caching_sha2'").rows
-
       options = {
         host: DEFAULT_HOST,
         port: DEFAULT_PORT,
@@ -94,6 +108,8 @@ class AuthTest < TrilogyTest
 
   def test_connection_error_native
     return skip unless has_native_password_plugin?
+    # mysql_native_password user creation has issues on MariaDB (connection reset during ALTER USER)
+    return skip("mysql_native_password test not supported on MariaDB") if is_mariadb?
     create_and_delete_test_user(username: "native", auth_plugin: "mysql_native_password") do
 
       err = assert_raises Trilogy::ConnectionError do
@@ -117,6 +133,7 @@ class AuthTest < TrilogyTest
   end
 
   def test_cleartext_auth_plugin_with_password
+    return skip unless has_cleartext_plugin_available?
     create_and_delete_test_user(username: "cleartext_user", auth_plugin: "cleartext_plugin_server") do
       client = new_tcp_client username: "cleartext_user", password: "password", enable_cleartext_plugin: true
       refute_nil client
@@ -126,6 +143,7 @@ class AuthTest < TrilogyTest
   end
 
   def test_cleartext_auth_plugin_disabled
+    return skip unless has_cleartext_plugin_available?
     create_and_delete_test_user(username: "cleartext_user", password: "", auth_plugin: "cleartext_plugin_server") do
 
       assert_raises Trilogy::AuthPluginError do
